53a489aa7b5a4837660ac66826ee18b6b32f0ecc
Medido1 <medi1222@gmail.com>
2025-04-15 19:18:30 +0100

    Create header for mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>
-      test
-    </h1>
+    <main className='min-h-[100vh] w-full font-[barlow]'>
+      <Header />
+    </main>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import bgMobile from "../assets/images/bg-header-mobile.png";
+import logo from "../assets/images/logo.svg";
+
+function Header() {
+  return (
+    <div className="h-40 flex flex-col items-center p-6 text-center"
+      style={{backgroundImage: `url(${bgMobile})`}}>
+        <img src={logo} alt="logo" />
+        <h1 className="mt-16 mb-4 text-3xl font-bold">
+          A history of everything you copy
+        </h1>
+        <p className="text-gray-400">
+          Clipboard allows you to track and organize everything you 
+          copy. Instantly access your clipboard on all your devices.
+        </p>
+        <button className="bg-teal-400 mt-4 p-2 rounded-full text-white w-full text-xl
+          font-bold shadow-md cursor-pointer">
+          Download for iOS
+        </button>
+        <button className="bg-indigo-400 mt-4 p-2 rounded-full text-white w-full text-xl
+          font-bold shadow-md cursor-pointer">
+          Download for Mac
+        </button>
+    </div>
+  )
+}
+
+export default Header;
